036d3fd584684686fafa828547515b4c86b80a05
박성준 <freestarjun@gmail.com>
2025-12-01 16:02:17 +0900

    feat : 파일구조 생성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import '../styles/Home.css'
+
+
+function Home() {
+    return (
+        <>
+            <div className="Main-Title">
+                <span>홍수</span>
+                <span>디딤이</span>
+            </div>
+        </>
+    )
+}
+export default Home
