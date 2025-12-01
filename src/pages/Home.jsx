2094b604855f91e511392e870d0ff60ceac77212
박성준 <freestarjun@gmail.com>
2025-12-01 20:09:19 +0900

    fix : footer창 value수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import '../styles/Home.css'
+import '../styles/footer.css'
 
 
 function Home() {
@@ -9,6 +10,12 @@ function Home() {
                 <span>홍수</span>
                 <span>디딤이</span>
             </div>
+
+            <footer className = "footer-container">
+                <img className="Car-Off" src="/Car-Off.svg" />
+                <img className="Bell-Off" src="/Bell-Off.svg" />
+                <img className="Setting-Off" src="/Setting-Off.svg" />
+            </footer>
         </>
     )
 }
